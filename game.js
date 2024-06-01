b4e7c9785e981dafbe05630fce0e2988c450f70e
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-06-01 16:09:36 -0700

    Create ball and movement variables

--- game.js
+++ game.js
@@ -1,20 +1,41 @@
 // Global Variables
 var canvas, canvasContext;
-
-console.log("test");
+var framesPerSecond = 30;
+var ballX = 75;
+var ballY = 75;
+var ballSpeedX = 5;
+var ballSpeedY = 10;
 
 // Main Game Code
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext("2d");
 
-    canvasContext.fillStyle="black";
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-
-    canvasContext.fillStyle = "white";
-    canvasContext.beginPath();
-    canvasContext.arc(100, 100, 10, 0, Math.PI*2, true);
-    canvasContext.fill();
+    setInterval(updateAll, 1000/framesPerSecond);
 }
 
+function updateAll() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
 
+    if(ballX < 0) {
+        ballSpeedX *= -1;
+    }
+    if(ballX > canvas.width) {
+        ballSpeedX *= -1;
+    }
+    if(ballY < 0) {
+        ballSpeedY *= -1;
+    }
+    if(ballY > canvas.height) {
+        ballSpeedY *= -1;
+    } 
+
+    canvasContext.fillStyle="black";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    
+    canvasContext.fillStyle = "white";
+    canvasContext.beginPath();
+    canvasContext.arc(ballX, ballY, 10, 0, Math.PI*2, true);
+    canvasContext.fill();
+}
