7eb49cfa0ac5b502ab3b53d76c18098f55a66ab5
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-06-01 16:15:46 -0700

    Create helper functions for cleaner code

--- game.js
+++ game.js
@@ -15,6 +15,11 @@ window.onload = function() {
 }
 
 function updateAll() {
+    moveAll();
+    drawAll();
+}
+
+function moveAll() {
     ballX += ballSpeedX;
     ballY += ballSpeedY;
 
@@ -30,12 +35,22 @@ function updateAll() {
     if(ballY > canvas.height) {
         ballSpeedY *= -1;
     } 
+}
 
-    canvasContext.fillStyle="black";
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+function drawAll() {
+    colorRect(0, 0, canvas.width, canvas.height, "black"); //clear screen
     
-    canvasContext.fillStyle = "white";
+    colorCircle(ballX, ballY, 10, "red"); //draw ball
+}
+
+function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor){
+    canvasContext.fillStyle = fillColor;
+    canvasContext.fillRect(topLeftX,topLeftY, boxWidth,boxHeight);
+}
+
+function colorCircle(centerX, centerY, radius, fillColor) {
+    canvasContext.fillStyle = fillColor;
     canvasContext.beginPath();
-    canvasContext.arc(ballX, ballY, 10, 0, Math.PI*2, true);
+    canvasContext.arc(centerX, centerY, radius, 0,Math.PI*2, true);
     canvasContext.fill();
 }
